608e9c8e48caa6ae0ec2f0b16b4e1468ffacaa04
mykola.diahovets <mykola.diahovets@digicert.com>
2024-05-18 00:29:10 +0300

    WIP

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({
-      auto_install = false,
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
+  	opts = {
+  		ensure_installed = {
         "json",
-        -- WEB dev
         "html",
         "css",
         "scss",
@@ -32,7 +10,6 @@ return {
         "typescript",
         "svelte",
         "tsx",
-        --
         "yaml",
         "prisma",
         "markdown",
@@ -52,16 +29,6 @@ return {
         "glsl",
         "objc",
         "toml"
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+  		},
+  	},
 }
